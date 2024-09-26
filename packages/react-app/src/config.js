c7c381b58e92a1b86396d1ce30212aa1f3aac43b
Hatterdev <120704180+Hatterdev@users.noreply.github.com>
2024-09-26 11:27:22 -0300

    Update config.js

--- packages/react-app/src/config.js
+++ packages/react-app/src/config.js
@@ -1,10 +1,10 @@
-import { Goerli } from "@usedapp/core";
+export const VITRA_CHAIN_ID = 350; // ID da cadeia VITRA
 
-export const ROUTER_ADDRESS = "0x6E71cfbc04eF15db5CcB942f2e4C78efCDD02833";
+export const ROUTER_ADDRESS = "0x6E71cfbc04eF15db5CcB942f2e4C78efCDD02833"; // Endereço do roteador na rede VITRA
 
 export const DAPP_CONFIG = {
-  readOnlyChainId: Goerli.chainId,
+  readOnlyChainId: VITRA_CHAIN_ID,
   readOnlyUrls: {
-    [Goerli.chainId]: "https://eth-goerli.g.alchemy.com/v2/VFVeT3J9yUX9SZs4FStpZUZSJyIUolda",
+    [VITRA_CHAIN_ID]: "https://vitrachain-rpc.com", // URL do RPC da rede VITRA
   },
 };
